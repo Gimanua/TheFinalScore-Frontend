c143af227820fd1104a32bbf922fc5263b318efb
Erik <43432787+ErikLager@users.noreply.github.com>
2019-12-12 10:34:47 +0100

    Table

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.css'
 import './index.css';
+import Table from './table';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Table />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/table.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import './App.css';
+
+
+
+export default function Table(){
+return (
+    <div className="List">
+      <h1>The Final Score</h1>
+      <table>
+        <thead>
+          <th>Title</th>
+          <th>Year</th>
+          <th>Actor</th>
+          <th>Plot</th>
+          <th>FINALSCORE</th>
+          <th>Type</th>
+        </thead>
+        <tbody>
+        <tr>
+            <td>
+              Star wars
+            </td>
+            <td>
+              1977
+            </td>
+            <td>
+              Mark Hamill, Carrie Fisher, Harrison Ford
+            </td>
+            <td>
+                Lorem ipsum
+            </td>
+            <td>
+              89,67
+            </td>
+            <td>
+              Movie
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Star wars
+            </td>
+            <td>
+              1977
+            </td>
+            <td>
+              Mark Hamill, Carrie Fisher, Harrison Ford
+            </td>
+            <td>
+                Lorem ipsum
+            </td>
+            <td>
+              89,67
+            </td>
+            <td>
+                Movie
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Star wars
+            </td>
+            <td>
+              1977
+            </td>
+            <td>
+              Mark Hamill, Carrie Fisher, Harrison Ford
+            </td>
+            <td>
+                Lorem ipsum
+            </td>
+            <td>
+              89,67
+            </td>
+            <td>
+                Movie
+            </td>
+          </tr>
+          <tr>
+            <td>
+              Star wars
+            </td>
+            <td>
+              1977
+            </td>
+            <td>
+              Mark Hamill, Carrie Fisher, Harrison Ford
+            </td>
+            <td>
+                Lorem ipsum
+            </td>
+            <td>
+              89,67
+            </td>
+            <td>
+                Movie
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
