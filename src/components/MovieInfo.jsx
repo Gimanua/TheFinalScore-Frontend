bd706f1d9bd01b87958128b951e2531894f70693
Matteus Psajd <43166440+Kap10-Diabeto@users.noreply.github.com>
2019-12-13 14:11:17 +0100

    MoviePage fixer

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -5,30 +5,20 @@ import FinalScore from './FinalScore';
 
 import './scss/MovieInfo.scss';
 
-export default function MovieInfo({title, synopsis, imgSrc, scores, finalScore}){
+export default function MovieInfo({title, synopsis, logo, scores, finalScore}){
 
-    const leftSideScores = [];
-    const rightSideScores = [];
-    scores.forEach((score, index) => {
-        if (index % 2 == 0) 
-            leftSideScores.push(score);
-        else
-            rightSideScores.push(score);
-    });
 
     return (
         <article id="movie-info" className="has-text-centered">
             <h2 className="has-text-weight-bold is-size-1">{title}</h2>
+            <img class="MovieImg" src={logo}></img>
             <h3 className="has-text-weight-bold is-size-4">Synopsis</h3>
             <p>{synopsis}</p>
-            <section className="columns">
-                <ul className="column is-two-fifths columns is-marginless">
-                    {leftSideScores.map((score, index) => <li className="column is-paddingless" key={index}><Score {...score} /></li>)}
+            <section>
+                <ul>
+                    {scores.map((score, index) => <li key={index}><Score {...score} /></li>)}
                 </ul>
                 <section className="column is-one-fifth is-marginless"><FinalScore value={finalScore} /></section>
-                <ul className="column is-two-fifths columns is-marginless">
-                    {rightSideScores.map((score, index) => <li className="column is-paddingless" key={index}><Score {...score} /></li>)}
-                </ul>
             </section>
         </article>
     );
