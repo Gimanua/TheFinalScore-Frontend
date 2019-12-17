b802a342b298c8c06a0f7804338c15abd3446c26
Matteus Psajd <43166440+Kap10-Diabeto@users.noreply.github.com>
2019-12-17 10:20:07 +0100

    moviepage progress

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -7,17 +7,25 @@ import './scss/MovieInfo.scss';
 
 export default function MovieInfo({title, synopsis, logo, scores, finalScore}){
     return (
+        <>
         <article id="movie-info" className="has-text-centered">
-            <h2 className="has-text-weight-bold is-size-1">{title}</h2>
+            <h2 className="has-text-weight-bold is-size-1">{title}(Year)</h2>
             <img className="MovieImg" src={logo}></img>
             <h3 className="has-text-weight-bold is-size-4">Synopsis</h3>
             <p>{synopsis}</p>
-            <section>
+            <br></br>
+            <h3 className="has-text-weight-bold is-size-6">Genres: Horror, Thriller, Comedy</h3>
+            <h3 className="has-text-weight-bold is-size-6">Director: Vladimir Putin</h3>
+            <h3 className="has-text-weight-bold is-size-6">Actors: Obama Obama, Mark Zuckerberg, Osama Bin Laden, He-Man </h3>
+            <section className="ratingElements">
                 <ul>
                     {scores.map((score, index) => <li key={index}><Score {...score} /></li>)}
                 </ul>
-                <section className="column is-one-fifth is-marginless"><FinalScore value={finalScore} /></section>
             </section>
         </article>
+<div class="finalScoreContainer">
+<section className="finalScore"><FinalScore value={finalScore} /></section>
+</div>
+</>
     );
 }
--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 export default function Score({value, sourceLogo}){
     return (
-        <section>
-            <p className="is-size-3">{value}</p>
-            <img src={sourceLogo} />
+        <section className="scores">
+            <p className="point">{value}</p>
+            <img classname="pointimg" src={sourceLogo} />
         </section>
     );
 }
