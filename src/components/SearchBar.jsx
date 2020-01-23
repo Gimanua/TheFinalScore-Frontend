ef6a2838361762c6498ac2fcf269bb0d592680f2
Erik <43432787+ErikLager@users.noreply.github.com>
2020-01-23 09:54:27 +0100

    Error fixed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ import "./scss/App.scss"
 export default function SearchBar({search}){
     return ( 
         <>
-        <input className="search" onfocus="this.value=''" onInput={(e) => search(e.target.value)} type="text" placeholder="Search"/>
+        <input className="search" onInput={(e) => search(e.target.value)} type="text" placeholder="Search"/>
         </>
     );
 }
