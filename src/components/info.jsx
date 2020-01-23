eba9ba74c0df412082c6faa72a06d8f08026138b
Matteus Psajd <43166440+Kap10-Diabeto@users.noreply.github.com>
2020-01-23 13:10:31 +0100

    Lite mer css fix

--- src/components/info.jsx
+++ src/components/info.jsx
@@ -4,7 +4,7 @@ import './scss/InfoPage.scss';
 export default function Info() {
     return (
       <div id="infodiv">
-        <h1>The Final Score</h1>
+        <h1 className="info-h1">The Final Score</h1>
         <p id="Info">This is a page where you can check the score of movies from sources such as IMDB, <br/>
         metacritic and rottentomatoes. Api that are used are <a href="https://www.omdbapi.com" target="_Blank">omdbapi.com</a> and <a href="https://api.themoviedb.org">themoviedb.org</a>.<br/>
         This api is used to get the info such as the title of the film,<br/>
